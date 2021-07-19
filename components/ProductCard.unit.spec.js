b91b5683c8d4f08cb6b09ca50c42227758185c0a
Gabriel <gabrielfranccelino@gmail.com>
2021-07-19 18:59:57 -0300

    test: server created mirageja

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -1,14 +1,30 @@
 import { mount } from '@vue/test-utils'
 import ProductCard from '@/components/ProductCard.vue'
+import { makeServer } from '@/miragejs/server'
 
 describe('ProductCard - unit', () => {
+  let server
+
+  beforeEach(() => {
+    server = makeServer({ environment: 'test' })
+  })
+
+  afterEach(() => {
+    server.shutdown()
+  })
+
   it('should mount the component', () => {
     const wrapper = mount(ProductCard, {
       propsData: {
-        product: {},
+        product: server.create('product', {
+          title: 'Relógio bonito',
+          price: '915,00',
+        }),
       },
     })
 
     expect(wrapper.vm).toBeDefined()
+    expect(wrapper.text()).toContain('Relógio bonito')
+    expect(wrapper.text()).toContain('915,00')
   })
 })
