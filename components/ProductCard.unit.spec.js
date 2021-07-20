ce68c7b9d9d9e22089d6cc9e9b55d327f40ddbbf
Gabriel <gabrielfranccelino@gmail.com>
2021-07-20 17:22:07 -0300

    test: component ProductToCart

--- components/ProductCard.unit.spec.js
+++ components/ProductCard.unit.spec.js
@@ -2,6 +2,21 @@ import { mount } from '@vue/test-utils'
 import ProductCard from '@/components/ProductCard.vue'
 import { makeServer } from '@/miragejs/server'
 
+const mountProductCard = (server) => {
+  const product = server.create('product', {
+    title: 'Relógio bonito',
+    price: '915,00',
+  })
+
+  const wrapper = mount(ProductCard, {
+    propsData: {
+      product,
+    },
+  })
+
+  return { wrapper, product }
+}
+
 describe('ProductCard - unit', () => {
   let server
 
@@ -14,17 +29,20 @@ describe('ProductCard - unit', () => {
   })
 
   it('should mount the component', () => {
-    const wrapper = mount(ProductCard, {
-      propsData: {
-        product: server.create('product', {
-          title: 'Relógio bonito',
-          price: '915,00',
-        }),
-      },
-    })
+    const { wrapper } = mountProductCard(server)
 
     expect(wrapper.vm).toBeDefined()
     expect(wrapper.text()).toContain('Relógio bonito')
     expect(wrapper.text()).toContain('915,00')
+  })
+
+  it('should emit the event addToCart with product object when button gets clicked', async () => {
+    const { wrapper, product } = mountProductCard(server)
+
+    await wrapper.find('button').trigger('click')
+
+    expect(wrapper.emitted().addToCart).toBeTruthy()
+    expect(wrapper.emitted().addToCart.length).toBe(1)
+    expect(wrapper.emitted().addToCart[0]).toEqual([{ product }])
   })
 })
